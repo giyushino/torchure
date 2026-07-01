c1fcbdda9194c214dc5e59ffd0520d728ac50b13
giyushino <allanzhang440@gmail.com>
2026-06-30 17:57:25 -0700

    wsd scheduler

---
+++ torchure/optimizer/scheduler.py
@@ -0,0 +1,45 @@
+"""
+learning rate scheduler
+functions are not typed, maybe we should change
+https://docs.pytorch.org/docs/2.12/generated/torch.optim.lr_scheduler.LambdaLR.html
+"""
+import torch
+
+from torch.optim.lr_scheduler import LRScheduler
+
+class WarmupStableDecaySchedulder(LRScheduler):
+    def __init__(self, optimizer, total_steps, warmup_ratio, decay_phase, peak_lr, last_epoch):
+        self.total_steps = total_steps
+        self.warmup_ratio = warmup_ratio
+        self.decay_phase = decay_phase
+        self.max_lr = peak_lr
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self) -> list[float | torch.Tensor]:
+        step = self.last_epoch
+        decay_phase_start = self.total_steps * (1 - self.decay_phase) 
+
+        if step <= self.total_steps * self.warmup_ratio:
+            scale = step / (self.total_steps * self.warmup_ratio)
+
+        elif step >= decay_phase_start:
+            num_decay_steps = self.decay_phase * self.total_steps
+            scale = 1 - (step - decay_phase_start / num_decay_steps)
+
+        else:
+            scale = 1.0
+
+        return [base_lr * scale for base_lr in self.base_lrs] 
+
+
+class CosineAnnealingScheduler(LRScheduler):
+    def __init__(self, optimizer, total_steps, warmup_ratio, decay_phase, peak_lr, last_epoch):
+        self.total_steps = total_steps
+        self.warmup_ratio = warmup_ratio
+        self.decay_phase = decay_phase
+        self.max_lr = peak_lr
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self) -> list[float | torch.Tensor]:
+        scale = 0 # temp
+        return [base_lr * scale for base_lr in self.base_lrs] 
